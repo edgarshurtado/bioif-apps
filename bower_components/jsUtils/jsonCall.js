083c25a28c69440591618037649c5a1d96a23683
Edgar S. Hurtado <edsanhu@gmail.com>
2016-04-27 12:38:56 +0200

    Extract json request from business logic. bz-collection as responsive

---
+++ bower_components/jsUtils/jsonCall.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Created by Edgar S. Hurtado on 27/04/16.
+ */
+
+
+
+function jsonRequest (url, cb){
+    var oReq = new XMLHttpRequest();
+
+    function reqListener() {
+        var jsonObject = JSON.parse(this.responseText);
+        cb(jsonObject);
+    }
+
+    oReq.open("GET", url);
+    oReq.send();
+
+    oReq.addEventListener("load", reqListener);
+    oReq.addEventListener("error", function(){alert("Server currently unavailable")});
+}
